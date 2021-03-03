6cd77c159b7042d7165ee9c350cfb3419a971f63
weklar10@gmail.com <weklar10@gmail.com>
2021-03-02 20:57:12 -0800

    service and controller

---
+++ ppmtool/src/main/java/pl/marcin/ppmtool/services/ProjectService.java
@@ -0,0 +1,21 @@
+package pl.marcin.ppmtool.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.marcin.ppmtool.domain.Project;
+import pl.marcin.ppmtool.repositories.ProjectRepository;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+
+        //Logic
+
+        return projectRepository.save(project);
+    }
+
+}
