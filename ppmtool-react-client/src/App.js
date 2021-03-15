0e198b3279dffa0ff78df0417e5720e9e3f9e870
weklar10@gmail.com <weklar10@gmail.com>
2021-03-15 13:40:38 -0700

    create addProject and CreateProjectButton

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,14 +3,19 @@ import "./App.css";
 import Dashboard from "./component/Dashboard";
 import Header from "./component/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import AddProject from "./component/Project/AddProject";
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
      <Header />
-        <Dashboard />
+     <Route exact path= "/dashboard" component={Dashboard} />
+     <Route exact path= "/addProject" component={AddProject} />
       </div>
+      </Router>
     );
   }
 }
