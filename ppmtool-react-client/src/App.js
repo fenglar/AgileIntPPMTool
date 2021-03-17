13896b56e6b293a6b68adc953264be36ab2a68f5
weklar10@gmail.com <weklar10@gmail.com>
2021-03-16 18:42:21 -0700

    create redux store

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -5,10 +5,14 @@ import Header from "./component/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import AddProject from "./component/Project/AddProject";
+import {Provider} from "react-redux";
+import store from "./store";
+
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <Router>
       <div className="App">
      <Header />
@@ -16,6 +20,7 @@ class App extends Component {
      <Route exact path= "/addProject" component={AddProject} />
       </div>
       </Router>
+      </Provider>
     );
   }
 }
