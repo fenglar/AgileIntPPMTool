f279da2cebb1a566c7572a29e52948652ab7af7c
weklar10@gmail.com <weklar10@gmail.com>
2021-03-22 22:09:58 -0700

    bug fix-update behaviour

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -5,13 +5,16 @@ import {GET_ERRORS,GET_PROJECT,GET_PROJECTS} from "./types";
 export const createProject = (project, history) => async dispatch => {
     try {
 const res= await axios.post
-("http://localhost:8080/api/project", project)
+("http://localhost:8080/api/project", project);
 history.push("/dashboard");
-
-    } catch (err) {
 dispatch({
     type:GET_ERRORS,
-    payload:err.response.data
+    payload:{}
+    });
+ } catch (err) {
+        dispatch({
+            type:GET_ERRORS,
+            payload:err.response.data
 });
     }
 };
