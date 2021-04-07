e0adaccf29444490662639e598473ca839183344
weklar10@gmail.com <weklar10@gmail.com>
2021-04-07 01:40:11 -0700

    handle UpdateProjectTask errors

--- ppmtool-react-client/src/component/ProjectBoard/Backlog.js
+++ ppmtool-react-client/src/component/ProjectBoard/Backlog.js
@@ -14,7 +14,6 @@ class Backlog extends Component {
       let doneItems = [];
 
       for(let i=0; i<tasks.length; i++){
-          console.log(tasks[i]);
 
          if(tasks[i].props.project_task.status ==="TO_DO"){
              todoItems.push(tasks[i]);
@@ -48,6 +47,7 @@ class Backlog extends Component {
                 <h3>In Progress</h3>
               </div>
             </div>
+            {inProgressItems}
             {
               //  <!-- SAMPLE PROJECT TASK STARTS HERE -->
               //         <!-- SAMPLE PROJECT TASK ENDS HERE -->
