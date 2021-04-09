da570d6e3e0c475ac64f8d8759f7837e9609dc72
weklar10@gmail.com <weklar10@gmail.com>
2021-04-08 21:38:17 -0700

    user registration part 2

--- ppmtool/src/main/java/pl/marcin/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
+++ ppmtool/src/main/java/pl/marcin/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -25,4 +25,11 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
         ProjectNotFoundExceptionResponse exceptionResponse=new ProjectNotFoundExceptionResponse(ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handlerUsernameAlreadyExists(UsernameAlreadyExistsException ex, WebRequest request)
+    {
+        UsernameAlreadyExistsResponse exceptionResponse=new UsernameAlreadyExistsResponse(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
 }
--- ppmtool/src/main/java/pl/marcin/ppmtool/services/UserService.java
+++ ppmtool/src/main/java/pl/marcin/ppmtool/services/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.marcin.ppmtool.domain.User;
+import pl.marcin.ppmtool.exceptions.UsernameAlreadyExistsException;
 import pl.marcin.ppmtool.repositories.UserRepository;
 
 @Service
@@ -16,12 +17,17 @@ public class UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User saveUser (User newUser){
-      newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
-      //username has to be unique (exception)
+        try {
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            //username has to be unique (exception)
+            newUser.setUsername(newUser.getUsername());
+            //make sure that password and confirmpassword match
+            //we dont persist or show the confirmpassword
+            return userRepository.save(newUser);
+        } catch (Exception e) {
+        throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists");
+        }
 
-        //make sure that password and confirmpassword match
-        //we dont persist or show the confirmpassword
-      return userRepository.save(newUser);
     }
 
 
